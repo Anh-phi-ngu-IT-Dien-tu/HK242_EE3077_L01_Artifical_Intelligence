9e4f909378ec0bbf98d00e61f63ff08a3bf0cc86
Anh_Phi_GIGABYTE <freeakaphi2k3@gmail.com>
2025-03-27 08:35:32 +0700

    Bai6

---
+++ Bai6/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "q_learning.h"
+#include "Eigen"
+
+int main() 
+{
+    Q_Learning path_planning(6,6,0.8);
+    Eigen::Matrix<double,6,6> R{
+        {-1,-1,-1,-1,0,-1},
+        {-1,-1,-1,0,-1,100},
+        {-1,-1,-1,0,-1,-1},
+        {-1,0,0,-1,0,-1},
+        {0,-1,-1,0,-1,100},
+        {-1,0,-1,-1,0,100}
+    };
+    path_planning.setReward(R);
+    path_planning.QLearningAgorithm();
+    std::cout<<path_planning;
+    return 0;    
+}
+
